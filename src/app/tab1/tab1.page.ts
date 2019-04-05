37e4f94fe3a004c8116acd82a0bf7046c3d83e11
Bryan Astacio <brya0196@gmail.com>
2019-04-05 10:19:53 -0400

    changed the tab1 and added the geolocation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+  temperature:number = 45;
+
+  constructor(private geolocation: Geolocation) {}
+
+  ngOnInit(): void {
+    this.geolocation.watchPosition().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+}
